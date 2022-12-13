d4cdc06b665f4802a4c5d28509110eb0465466b6
baran <baturbarann@gmail.com>
2022-12-13 18:09:20 +0300

    asdas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const path = require('path')
+const cors = require('cors')
 require('dotenv').config()
 
 
@@ -8,6 +9,9 @@ const app = express()
 app.get('/', (req, res, next) => {
     res.json("hello")
 })
+
+// Enable CORS
+app.use(cors())
 // Enable body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
